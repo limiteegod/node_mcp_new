15bf97b6df4abb8c6f4ba4d16bf1ae46b2a34c55
limiteegod <limiteemail@163.com>
2014-09-29 18:48:10 +0800

    transaction success.

--- app/service/OrderService.js
+++ app/service/OrderService.js
@@ -2,6 +2,7 @@ var async = require('async');
 var prop = require('../config/Prop.js');
 var db = require('../config/McpDataBase.js');
 var errCode = require('../config/ErrCode.js');
+var transaction = require('./Transaction.js');
 
 var OrderService = function(){};
 
@@ -9,87 +10,67 @@ OrderService.prototype.incSuccessTicketCount = function(orderId, cb)
 {
     var self = this;
     var orderTable = db.get("torder");
-    var success = false;
-    async.whilst(
-        function() { return !success},
-        function(whileCb) {
-            async.waterfall([
-                //find the order
-                function(cb){
-                    orderTable.find({id:orderId}, {}).toArray(function(err, data){
-                        if(err) throw err;
-                        if(data.length != 1)
-                        {
-                            cb(errCode.E2005);
-                        }
-                        else
-                        {
-                            cb(null, data[0]);
-                        }
-                    });
-                },
-                //check the status
-                function(torder, cb){
-                    if(torder.status != prop.orderStatus.waiting_print)
+    transaction.run(function(wCb){
+        async.waterfall([
+            //find the order
+            function(cb){
+                orderTable.find({id:orderId}, {}).toArray(function(err, data){
+                    if(err) throw errCode.E9999;
+                    if(data.length != 1)
                     {
-                        cb(errCode.E2057);
+                        cb(errCode.E2005);
                     }
                     else
                     {
-                        cb(null, torder);
+                        cb(null, data[0]);
                     }
-                },
-                //increase the ticketcount
-                function(torder, cb)
+                });
+            },
+            //check the status
+            function(torder, cb){
+                if(torder.status != prop.orderStatus.waiting_print)
                 {
-                    torder.printCount = torder.printCount + 1;
-                    if(torder.printCount + torder.printFailCount >= torder.ticketCount)
-                    {
-                        if(torder.printFailCount > 0)
-                        {
-                            torder.status = prop.orderStatus.partial_success;
-                        }
-                        else
-                        {
-                            torder.status = prop.orderStatus.success;
-                        }
-                    }
-                    orderTable.update({id:torder.id, version:torder.version},
-                        {$inc:{printCount:torder.printCount}, $set:{status:torder.status}}, {}, function(err, data){
-                        if(err) throw err;
-                        if(data.updateCount == 1)
-                        {
-                            success = true;
-                            cb(null, torder);
-                        }
-                        else
-                        {
-                            cb(null);
-                        }
-                    });
-                }
-            ], function (err, result) {
-                if(err)
-                {
-                    if(err != errCode.E9999)    //err except E9999, to up callback
-                    {
-                        whileCb(err, result);
-                    }
-                    else
-                    {
-                        whileCb(null, result);
-                    }
+                    cb(errCode.E2057);
                 }
                 else
                 {
-                    whileCb(null, result);
+                    cb(null, torder);
                 }
-            });
-        },
-        function(err) {
-            cb(err);
-        }
-    );
+            },
+            //increase the ticketcount
+            function(torder, cb)
+            {
+                torder.printCount = torder.printCount + 1;
+                if(torder.printCount + torder.printFailCount >= torder.ticketCount)
+                {
+                    if(torder.printFailCount > 0)
+                    {
+                        torder.status = prop.orderStatus.partial_success;
+                    }
+                    else
+                    {
+                        torder.status = prop.orderStatus.success;
+                    }
+                }
+                orderTable.update({id:torder.id, version:torder.version},
+                    {$inc:{printCount:torder.printCount}, $set:{status:torder.status}}, {}, function(err, data){
+                        if(err) throw errCode.E9999;
+                        if(data.updateCount == 1)
+                        {
+                            cb(errCode.E0000, torder);
+                        }
+                        else
+                        {
+                            cb(errCode.E9999);
+                        }
+                    });
+            }
+        ], function (err, result) {
+            wCb(err, result);
+        });
+    }, function(err, data){
+        cb(err, data);
+    });
 };
 
 module.exports = new OrderService();
