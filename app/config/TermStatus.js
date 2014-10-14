2ea26a933774bf34c36f676af7097509b90f540b
LiMing <liming@localhost.localdomain>
2014-10-14 18:18:28 +0800

    more term status

--- app/config/TermStatus.js
+++ app/config/TermStatus.js
@@ -3,7 +3,15 @@ var TermStatus = function(){
     self.info = [{id:1000, code:'INIT', des:'初始状态'},
         {id:1100, code:'NOT_ON_SALE', des:'未开售'},
         {id:1150, code:'PRE_ON_SALE', des:'准备开售中'},
-        {id:1200, code:'ON_SALE', des:'正在销售'}];
+        {id:1200, code:'ON_SALE', des:'正在销售'},
+        {id:1300, code:'PAUSE', des:'销售暂停'},
+        {id:1390, code:'PREEND', des:'销售结束中'},
+        {id:1400, code:'END', des:'销售结束'},
+        {id:1450, code:'SEND', des:'后台销售结束'},
+        {id:1460, code:'SYNCHRONIZING', des:'销售同步中'},
+        {id:1470, code:'SYNCHRONIZED', des:'销售已同步'},
+        {id:1480, code:'DRAW_EXPORT_DATA', des:'导出数据中'},
+        {id:1490, code:'WAITING_DRAW_NUMBER', des:'等待录入开奖号码'}];
     self.infoArray = {};
     self.init();
 };
