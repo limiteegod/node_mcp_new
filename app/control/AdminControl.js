3741ac50c4460059d9f6971c6551032ec57114dd
LiMing <liming@localhost.localdomain>
2014-10-14 14:25:28 +0800

    add station version

--- app/control/AdminControl.js
+++ app/control/AdminControl.js
@@ -186,6 +186,7 @@ AdminControl.prototype.handleAD03 = function(user, headNode, bodyNode, cb)
     station.queueIndex = -1;
     station.relayable = -1;
     station.status = stationStatus.OPEN.id;
+    station.version = 0;
     var table = dc.main.get("station");
     table.save(station, [], function(err, data){
         if(err)
