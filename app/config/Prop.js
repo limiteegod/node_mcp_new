351348999d3697a4ef3f9638c13327c75d809116
LiMing <liming@localhost.localdomain>
2014-10-27 12:34:06 +0800

    tmp

--- app/config/Prop.js
+++ app/config/Prop.js
@@ -1,4 +1,4 @@
-var esdb = require('easy_db');
+//var esdb = require('easy_db');
 var target = 'dev';
 var exports = {};
 
@@ -164,7 +164,7 @@ var dbs = [{
         'port':3306,
         'database':'mcp'
     },
-    type:esdb.prop.dbType.mysql,
+    //type:esdb.prop.dbType.mysql,
     dateToLong:false
 }, {
     //oracle dev
@@ -174,16 +174,16 @@ var dbs = [{
         database: "lottery", // System ID (SID)
         user: "liming",
         password: "0okmnhy6"
-    },
-    type:esdb.prop.dbType.oracle
+    }
+    //type:esdb.prop.dbType.oracle
 }, {
     //dev
-    config:{'url':'mongodb://127.0.0.1:27017/mcp'},
-    type:esdb.prop.dbType.mongodb
+    config:{'url':'mongodb://127.0.0.1:27017/mcp'}
+    //type:esdb.prop.dbType.mongodb
 }, {
     //run
-    config:{'url':'mongodb://192.168.222.233:27017/mcp'},
-    type:esdb.prop.dbType.mongodb
+    config:{'url':'mongodb://192.168.222.233:27017/mcp'}
+    //type:esdb.prop.dbType.mongodb
 }, {
     //yun test
     config:{
@@ -193,12 +193,12 @@ var dbs = [{
         'port':3306,
         'database':'mcp'
     },
-    type:esdb.prop.dbType.mysql,
+    //type:esdb.prop.dbType.mysql,
     dateToLong:false
 }, {
     //yun test
-    config:{'url':'mongodb://10.131.172.66:27017/mcp'},
-    type:esdb.prop.dbType.mongodb
+    config:{'url':'mongodb://10.131.172.66:27017/mcp'}
+    //type:esdb.prop.dbType.mongodb
 }];
 exports.dbs = dbs;
 
