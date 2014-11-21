45751c8e4f3d51ebb155df3bdc4e0d31739fe16b
LiMing <liming@localhost.localdomain>
2014-11-21 09:58:28 +0800

    出票成功之后，保存到成功队列

--- helper/AddOperation.js
+++ helper/AddOperation.js
@@ -48,6 +48,8 @@ var run = function()
                 });
                 operationTable.save({userType:userType.ADMINISTRATOR, id:'ADMIN_LIST_DRAW_TICKET', parent:'ADMIN_SALE', name:'算奖队列', url:'ticket_drawList.html', hasChildren:0}, [], function(err, data){
                 });
+                operationTable.save({userType:userType.ADMINISTRATOR, id:'ADMIN_LIST_SUC_TICKET', parent:'ADMIN_SALE', name:'成功队列', url:'ticket_sucList.html', hasChildren:0}, [], function(err, data){
+                });
             });
             operationTable.save({userType:userType.ADMINISTRATOR, id:'ADMIN_STATION', name:'机构管理', url:'', hasChildren:1, parent:''}, [], function(err, data){
                 operationTable.save({userType:userType.ADMINISTRATOR, id:'ADMIN_LIST_STATION', parent:'ADMIN_STATION', name:'机构列表', url:'station_list.html', hasChildren:0}, [], function(err, data){
