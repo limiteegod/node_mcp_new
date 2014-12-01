c756ac934634d049c8d4ef1f9f25b43ebf940284
LiMing <liming@localhost.localdomain>
2014-12-01 16:17:49 +0800

    期次封存信息可按时间查询

--- Tmp.js
+++ Tmp.js
@@ -1,29 +1,53 @@
 var async = require('async');
-var esut = require('easy_util');
+var CronJob = require("cron").CronJob;
 
-
-var dc = require('mcp_dao').dc;
+var platInterUtil = require('mcp_util').platInterUtil;
+var esut = require("easy_util");
 var log = esut.log;
+var digestUtil = esut.digestUtil;
 
-var addAccount = function()
+var cons = require('mcp_cons');
+var ticketStatus = cons.ticketStatus;
+var printStatus = cons.printStatus;
+var userType = cons.userType;
+
+var QueryTest = function(){
+    var self = this;
+    self.userId = 'Q0003';
+    self.channelCode = 'Q0003';
+    self.userType = 2;
+    self.key = 'cad6011f5f174a359d9a36e06aada07e';
+};
+
+QueryTest.prototype.query = function(cmd, bodyNode, cb)
 {
-    async.waterfall([
-        function(cb){
-            dc.init(function(err){
-                cb(err);
-            });
-        },
-        function(cb)
+    var self = this;
+    platInterUtil.get(self.userId, self.userType, self.channelCode, "md5", self.key, cmd, bodyNode, cb);
+};
+
+QueryTest.prototype.queryQ17 = function(bodyNode, cb)
+{
+    var self = this;
+    self.query("Q17", bodyNode, function(err, backMsgNode){
+        if(err)
         {
-            var table = dc.main.get("station");
-            table.update({code:"Q0003"}, {$set:{balance:10000000000}}, [], function(err, data){
-                cb(err, "success");
-            });
+            cb(err, null);
         }
-    ], function (err, result) {
-        log.info(err);
-        log.info("end...........");
+        else
+        {
+            var backBodyStr = digestUtil.check(backMsgNode.head, self.key, backMsgNode.body);
+            var backBodyNode = JSON.parse(backBodyStr);
+            cb(null, backBodyNode);
+        }
     });
 };
 
-addAccount();
+var queryTest = new QueryTest();
+queryTest.queryQ17({}, function(err, backBodyNode){
+    log.info(err);
+    log.info(backBodyNode);
+});
+
+
+
+
